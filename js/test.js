5b1368fe85502b365de9d06c4aea9d17dfb759dd
Emmanuel Cruz <emmanuel.cb@outlook.com>
2017-08-31 17:20:49 -0500

    08-31-2017 by Emmanuel CB

---
+++ js/test.js
@@ -0,0 +1,316 @@
+class ServiciosLienzo {
+	constructor(contenedor,herramientas) {
+		this.validarParametros(contenedor,herramientas);
+		this.inicializar(contenedor,herramientas);
+		this.agregarEventos(herramientas);
+	}
+	validarParametros(contenedor,herramientas) {
+		if(contenedor === undefined)
+			throw new Error('Ups!, parece que tu desarrollador la cago, no hay parentElement definido.');
+		if(Object.prototype.toString.call(herramientas) !== '[object Object]')
+			throw new Error('Ups!, parece que tu desarrollador la cago, este no es un Object.');
+	}
+	inicializar(contenedor,herramientas) {
+		this.contenedor = contenedor;
+		this.crearLienzo();
+		this.trazos = {};
+		this.count = 0;
+		this.trazoActivo = undefined;
+		this.herramientasPermitidas = ['seleccion','selecciondirecta','pluma','plumaeliminar'];
+		this.herramientas = {
+			seleccion : {
+				valor: 0, 
+				nombre: 'Seleccion',
+				abajo: function(este,evento) {},
+				arriba: function(este,evento) {}
+			},
+			selecciondirecta : {
+				valor: 1, 
+				nombre: 'Seleccion directa',
+				abajo: function(este,evento) {},
+				arriba: function(este,evento) {}
+			},
+			mover : {
+				valor: 2,
+				nombre: 'Mover',
+				abajo: function(este,evento) {},
+				arriba:function(este,evento) {}
+			},
+			pluma : {
+				valor: 3,
+				nombre: 'Pluma',
+				abajo: function(este,evento) {
+					var pos = este.obtenerPosicion(evento);
+					if(este.trazoActivo !== undefined)
+						este.trazoActivo.agregarPunto(pos);
+					else {
+						este.agregarTrazo(pos);
+					}
+				},
+				arriba: function(este,evento) {}
+			},
+			plumaeliminar : {
+				valor: 4,
+				nombre: 'Pluma eliminar puntos',
+				abajo: function(este,evento) {},
+				arriba: function(este,evento) {}
+			}
+		}
+		this.herramientaActiva = this.herramientas.pluma;
+		this.atajos = {
+			V : 'seleccion',
+			A : 'selecciondirecta',
+			P : 'pluma',
+			AltP : 'plumaeliminar'
+		}
+	}
+	crearLienzo(width,height) {
+		var imageSVG = document.createElementNS('http://www.w3.org/2000/svg','svg');
+		imageSVG.setAttributeNS(null,'width',20);
+		imageSVG.setAttributeNS(null,'height',20);
+		var ubicacionesX = [0,10,0,10];
+		var ubicacionesY = [0,0,10,10];
+		var colores = ['rgb(200,200,200)','rgb(255,255,255)','rgb(255,255,255)','rgb(200,200,200)'];
+		for(var i=0,j=4; i<j; i++) {
+			var cuadrado = document.createElementNS('http://www.w3.org/2000/svg','rect');
+			cuadrado.setAttributeNS(null,'x',ubicacionesX[i]);
+			cuadrado.setAttributeNS(null,'y',ubicacionesY[i]);
+			cuadrado.setAttributeNS(null,'width',10);
+			cuadrado.setAttributeNS(null,'height',10);
+			cuadrado.setAttributeNS(null,'fill',colores[i]);
+			imageSVG.appendChild(cuadrado);
+		}
+		var serializador = new XMLSerializer();
+		var fuente = serializador.serializeToString(imageSVG);
+		if(!fuente.match(/^<svg[^>]+xmlns="http\:\/\/www\.w3\.org\/2000\/svg"/))
+		    fuente = fuente.replace(/^<svg/, '<svg xmlns="http://www.w3.org/2000/svg"');
+		if(!fuente.match(/^<svg[^>]+"http\:\/\/www\.w3\.org\/1999\/xlink"/))
+		    fuente = fuente.replace(/^<svg/, '<svg xmlns:xlink="http://www.w3.org/1999/xlink"');
+		if(fuente.match(/xmlns="http\:\/\/www\.w3\.org\/1999\/xhtml"/))
+			fuente = fuente.replace(/xmlns="http\:\/\/www\.w3\.org\/1999\/xhtml"/, '');
+		fuente = '<?xml version="1.0" standalone="no"?>\r\n' + fuente;
+		var url = "data:image/svg+xml;charset=utf-8,"+encodeURIComponent(fuente);
+
+		var nuevoLienzo = document.createElementNS('http://www.w3.org/2000/svg','svg');
+		nuevoLienzo.setAttributeNS(null,'id','lienzo');
+		nuevoLienzo.setAttributeNS(null,'style','background:url("'+url+'");margin: 0 auto;');
+		this.contenedor.appendChild(nuevoLienzo);
+		this.lienzo = nuevoLienzo;
+	}
+	agregarEventos(herramientas) {
+		// General
+		this.lienzo.addEventListener('mousedown',this.manejadorRatonPresionado(this),false);
+		this.lienzo.addEventListener('mouseup',this.manejadorRatonSuelto(this),false);
+		// Atajos
+		document.addEventListener('keydown',this.agregarAtajoFuncion(this),false);
+		// Herramientas
+		for(var herramienta in herramientas) {
+			if(this.herramientasPermitidas.indexOf(herramienta) !== -1) {
+				herramientas[herramienta].addEventListener('click',this.agregaFuncion(this,herramienta),false);
+			}
+		}
+	}
+	manejadorRatonPresionado(este) {
+		var ratonPresionadoFn = function(evento){
+			if(evento.which == 1)
+				este.herramientaActiva.abajo(este,evento);
+
+		};
+		return ratonPresionadoFn;
+	}
+	manejadorRatonSuelto(este) {
+		var ratonSueltoFn = function(evento){
+			if(evento.which == 1)
+				este.herramientaActiva.arriba(este,evento);
+		};
+		return ratonSueltoFn;
+	}
+	agregarAtajoFuncion(este) {
+		var fn = function(evento) {
+			var tecla = evento.key;
+			if(este.atajos[tecla.toUpperCase()]) {
+				var herramienta = este.atajos[tecla.toUpperCase()];
+				este.herramientaActiva = este.herramientas[herramienta];
+			}
+		}
+		return fn;
+	}
+	agregaFuncion(este,herramienta) {
+		var fn = function(evento){
+			este.herramientaActiva = este.herramientas[herramienta];
+		}
+		return fn;
+	}
+	obtenerPosicion(evento) {
+		var x;
+		var y;
+		if (evento.pageX != undefined && evento.pageY != undefined) {
+			x = evento.pageX;
+			y = evento.pageY;
+		} else {
+			x = evento.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+			y = evento.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+		}
+		if(!isNaN(this.lienzo.offsetLeft) && !isNaN(this.lienzo.offsetTop)) {
+			x -= this.lienzo.offsetLeft;
+			y -= this.lienzo.offsetTop;
+		} else if(!isNaN(this.lienzo.parentElement.offsetLeft) && !isNaN(this.lienzo.parentElement.offsetTop)) {
+			x -= this.lienzo.parentElement.offsetLeft;
+			y -= this.lienzo.parentElement.offsetTop;
+		}
+		
+		return {x:x,y:y};
+	}
+	agregarTrazo(pos) {
+		this.trazos[this.count] = new Trazo(this.count,pos,this);
+		this.trazoActivo = this.trazos[this.count];
+		this.count++;
+	}
+}
+class Trazo {
+	constructor(id,pos,servicios) {
+		this.validarParametros(id,pos,servicios);
+		this.id = id;
+		this.puntos = {};
+		this.count = 0;
+		this.puntoActivo;
+		this.servicios = servicios;
+		this.agregarPunto(pos);
+	}
+	validarParametros(id,pos,servicios) {
+		if(id == null || id === undefined)
+			throw new Error('Un nuevo trazo require de un id.');
+		if(pos == null || pos === undefined)
+			throw new Error('Un nuevo trazo require de unas coordenadas.');
+		if(servicios == null || servicios === undefined)
+			throw new Error('Un nuevo trazo require de un servicio.');
+	}
+	agregarPunto(pos) {
+		var hayInicial = this.puntos.length > 0 ? false : true;
+		this.puntos[this.count] = new Punto(this.count, pos.x, pos.y, hayInicial, this);
+		this.puntoActivo = this.puntos[this.count];
+		this.count++;
+	}
+}
+class Punto {
+	constructor(id,x,y,inicial,trazo) {
+		this.validarParametros(id,x,y,inicial,trazo);
+		this.id = id;
+		this.x = x;
+		this.y = y;
+		this.inicial = inicial;
+		this.bezierAnt = undefined;
+		this.bezierSig = undefined;
+		this.trazo = trazo;
+		this.radio = 3;
+		this.dibujarPunto();
+	}
+	validarParametros(id,x,y,inicial,trazo) {
+		if(id == null || id === undefined)
+			throw new Error('Para crear un nuevo punto requires de un id.');
+		if(x == null || x === undefined)
+			throw new Error('Para crear un nuevo punto requires de una coordenada x.');
+		if(y == null || y === undefined)
+			throw new Error('Para crear un nuevo punto requires de una coordenada y.');
+		if(inicial == null || inicial === undefined)
+			throw new Error('Para crear un nuevo punto necesitas especificar si es el punto inicial.');
+		if(trazo == null || trazo === undefined)
+			throw new Error('Un nuevo punto require de un trazo definido.');
+	}
+	dibujarPunto() {
+		var color = 'black';
+		var cuadrado = document.createElementNS('http://www.w3.org/2000/svg','rect');
+		cuadrado.setAttributeNS(null,'x',this.x-this.radio);
+		cuadrado.setAttributeNS(null,'y',this.y-this.radio);
+		cuadrado.setAttributeNS(null,'width',this.radio*2);
+		cuadrado.setAttributeNS(null,'height',this.radio*2);
+		cuadrado.setAttributeNS(null,'fill',color);
+		this.trazo.servicios.lienzo.appendChild(cuadrado);
+	}
+	agregarBezier(tipoBezier,punto) {
+		if(tipoBezier == 'anterior')
+			this.bezierAnt = new Bezier(this.x+5,this.y+5,this);
+		else
+			this.bezierSig = new Bezier(this.x+5,this.y+5,this);
+	}
+}
+class Bezier {
+	constructor(cx,cy,punto) {
+		this.cx = cx;
+		this.cy = cy;
+		this.punto = punto;
+		this.dibujarBezier();
+	}
+	dibujarBezier() {
+		var circulo = document.createElementNS('http://www.w3.org/2000/svg','circle');
+		circulo.setAttributeNS(null,'cx',this.x);
+		circulo.setAttributeNS(null,'cy',this.y);
+		circulo.setAttributeNS(null,'r',this.radio);
+		circulo.setAttributeNS(null,'fill',color);
+		this.punto.trazo.servicios.lienzo.appendChild(circulo);
+	}
+}
+
+var trazo = {
+	puntos: [
+		{
+			x: 200,
+			y: 100,
+			inicial: true,
+			bezierAnt: undefined,
+			bezierSig: {
+				cx: 250,
+				cy: 100
+			}
+		},
+		{
+			x: 300,
+			y: 200,
+			inicial: false,
+			bezierAnt: {
+				cx: 300,
+				cy: 150
+			},
+			bezierSig: {
+				cx: 300,
+				cy: 250
+			}
+		},
+		{
+			x: 200,
+			y: 300,
+			inicial: false,
+			bezierAnt: {
+				cx: 250,
+				cy: 300
+			},
+			bezierSig: {
+				cx: 150,
+				cy: 300
+			}
+		},
+		{
+			x: 100,
+			y: 200,
+			inicial: false,
+			bezierAnt: {
+				cx: 100,
+				cy: 250
+			},
+			bezierSig: {
+				cx: 100,
+				cy: 150
+			}
+		},
+		{
+			x: 200,
+			y: 100,
+			inicial: false,
+			bezierAnt: {
+				cx: 150,
+				cy: 100
+			},
+			bezierSig: undefined
+		}
+	]
+};
